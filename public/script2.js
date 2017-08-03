6a9d0ac6954586d6b624ea58dc4150cae00ca413
Andrew Tran <tran.andrew.c@gmail.com>
2017-08-03 15:50:49 -0600

    bound to all known form types, now persisting data

--- public/script2.js
+++ public/script2.js
@@ -1,28 +1,3 @@
-function bindToClick() {
-    $('body').bind('click', function (e) {
-        var selectedForm = $(e.target).closest('form');
-        selectedForm.css('border', '5px solid #ff0000');
-        $('body').unbind('click');
-        selectedForm.submit(function (evt) {
-            evt.preventDefault();
-            debugger;
-        })
-    });
-}
-
-function checkForKnownForms() {
-    $('body').click(function (evt) {
-        console.log($(evt.target).closest('form').serializeArray());
-    });
-}
-
-function bindToEmbededForm() {
-    $('#BVRRForm').on('submit', function (evt) {
-        evt.preventDefault();
-        $(this).serializeArray();
-    })
-}
-
 var username = "REPLACEPAYLOAD";
-$('body').append('<div id="weespringUsername" style="display: none;">'+username+'</div>');
-$('body').append('<script src="https://tacothursdaysandbox-38281.firebaseapp.com/reviewloader.js" type="text/javascript"><\/script>');
+jQuery('body').append('<div id="weespringUsername" style="display: none;">'+username+'</div>');
+jQuery('body').append('<script src="https://tacothursdaysandbox-38281.firebaseapp.com/reviewloader.js" type="text/javascript"><\/script>');
